bc5c32b24bf5949ea4740cae71f0a7d6950034b2
VVIMT <VVIMT@Vincents-MacBook-Air.local>
2017-09-27 01:27:40 +0200

    filler

--- flags.c
+++ flags.c
@@ -1,18 +1,28 @@
 #include "filler.h"
 
-long	tmp_distance(t_struct *t, char enemy)
+long	choose_targets(t_struct *t)
 {
-	sweeph1(t);
-	sweeph2(t);
-	sweepv1(t);
-	sweepv2(t);
-	return (ft_power(t->dis_hor, 2) + ft_power(t->dis_ver, 2));
+	int	a;
+	int	b;
+
+	a = 0;
+	b = 0;
+	if (sweeph1(t) > sweeph2(t))
+		a = 1;
+	else
+		a = 2;
+	if (sweepv1(t) > sweepv2(t))
+		b = 4;
+	else
+		b = 8;
+	return (a + b);
 }
 
 int		sweeph1(t_struct *t)
 {
 	int	sig1;
 	int	sig2;
+	int	sig3;
 	int	i;
 	int	j;
 
@@ -21,15 +31,28 @@ int		sweeph1(t_struct *t)
 	j = 0;
 	while (t->grid[0][j])
 	{
+		sig3 = 0;
 		i = -1;
 		while (t->grid[++i])
 		{
+			if (t->grid[i][j] == t->c || t->grid[i][j] == t->c_maj)
+				sig3 = 1;
 			if (t->grid[i][j] == t->enemy
 			|| t->grid[i][j] == t->enemy - 32)
 				sig2 = 1;
 		}
-		if (sig2 == 1)
-			break ;
+		if (sig2 == 1 && sig1 == 1 && j > 0)
+		{
+			if (t->grid[0][j - 1] == '.')
+				t->grid[0][j - 1] = '1';
+			if (t->grid[t->h_plateau - 1][j - 1] == '.')
+				t->grid[t->h_plateau - 1][j - 1] = '2';
+			return (j);
+		}
+		else if (sig2 == 1)
+			return (0);
+		if (sig3 == 1)
+			sig1 = 1;
 		j++;
 	}
 	return (0);
@@ -39,6 +62,7 @@ int		sweeph2(t_struct *t)
 {
 	int	sig1;
 	int	sig2;
+	int	sig3;
 	int	i;
 	int	j;
 
@@ -47,15 +71,28 @@ int		sweeph2(t_struct *t)
 	j = t->l_plateau - 1;
 	while (j >= 0)
 	{
+		sig3 = 0;
 		i = -1;
 		while (t->grid[++i])
 		{
+			if (t->grid[i][j] == t->c || t->grid[i][j] == t->c_maj)
+				sig3 = 1;
 			if (t->grid[i][j] == t->enemy
 			|| t->grid[i][j] == t->enemy - 32)
 				sig2 = 1;
 		}
-		if (sig2 == 1)
-			break ;
+		if (sig2 == 1 && sig1 == 1 && j < t->l_plateau - 1)
+		{
+			if (t->grid[0][j + 1] == '.')
+				t->grid[0][j + 1] = '3';
+			if (t->grid[t->h_plateau - 1][j + 1] == '.')
+				t->grid[t->h_plateau - 1][j + 1] = '4';
+			return (j);
+		}
+		else if (sig2 == 1)
+			return (0);
+		if (sig3 == 1)
+			sig1 = 1;
 		j--;
 	}
 	return (0);
@@ -65,6 +102,7 @@ int		sweepv1(t_struct *t)
 {
 	int	sig1;
 	int	sig2;
+	int	sig3;
 	int	i;
 	int	j;
 
@@ -73,15 +111,28 @@ int		sweepv1(t_struct *t)
 	i = 0;
 	while (t->grid[i])
 	{
+		sig3 = 0;
 		j = -1;
 		while (t->grid[i][++j])
 		{
+			if (t->grid[i][j] == t->c || t->grid[i][j] == t->c_maj)
+				sig3 = 1;
 			if (t->grid[i][j] == t->enemy
 			|| t->grid[i][j] == t->enemy - 32)
 				sig2 = 1;
 		}
-		if (sig2 == 1)
-			break ;
+		if (sig2 == 1 && sig1 == 1 && i > 0)
+		{
+			if (t->grid[i - 1][t->l_plateau] == '.')
+				t->grid[i - 1][t->l_plateau] = '5';
+			if (t->grid[i - 1][0] == '.')
+				t->grid[i - 1][0] = '6';
+			return (i);
+		}
+		else if (sig2 == 1)
+			return (0);
+		if (sig3 == 1)
+			sig1 = 1;
 		i++;
 	}
 	return (0);
@@ -91,6 +142,7 @@ int		sweepv2(t_struct *t)
 {
 	int	sig1;
 	int	sig2;
+	int	sig3;
 	int	i;
 	int	j;
 
@@ -99,15 +151,28 @@ int		sweepv2(t_struct *t)
 	i = t->h_plateau - 1;
 	while (i >= 0)
 	{
+		sig3 = 0;
 		j = -1;
 		while (t->grid[i][++j])
 		{
+			if (t->grid[i][j] == t->c || t->grid[i][j] == t->c_maj)
+				sig3 = 1;
 			if (t->grid[i][j] == t->enemy
 			|| t->grid[i][j] == t->enemy - 32)
 				sig2 = 1;
 		}
-		if (sig2 == 1)
-			break ;
+		if (sig2 == 1 && sig1 == 1 && i < t->h_plateau - 1)
+		{
+			if (t->grid[i + 1][t->l_plateau] == '.')
+				t->grid[i + 1][t->l_plateau] = '7';
+			if (t->grid[i + 1][0] == '.')
+				t->grid[i + 1][0] = '8';
+			return (i);
+		}
+		else if (sig2 == 1)
+			return (0);
+		if (sig3 == 1)
+			sig1 = 1;
 		i--;
 	}
 	return (0);
