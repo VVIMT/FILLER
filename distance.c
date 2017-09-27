c3d4f8f4b5d96a3c07593ce71bdc9c82626f5eb6
vinvimo <vinvimo@student.42.fr>
2017-09-27 22:29:01 +0200

    filler

--- distance.c
+++ distance.c
@@ -6,7 +6,7 @@
 /*   By: vinvimo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/18 22:56:16 by vinvimo           #+#    #+#             */
-/*   Updated: 2017/09/18 22:56:18 by vinvimo          ###   ########.fr       */
+/*   Updated: 2017/09/27 22:27:44 by vinvimo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,13 @@
 long	distance(t_struct *t)
 {
 	return (ft_power(t->dis_x, 2) + ft_power(t->dis_y, 2));
+}
+
+int		greatest(int a, int b)
+{
+	if (a > b)
+		b = a;
+	else if (a <= b)
+		a = b;
+	return (a);
 }
